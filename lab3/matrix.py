3d639c59b86246520ec34b490270fa93b40b319b
Daniel <danne_1998@hotmail.se>
2021-08-17 13:18:13 +0200

    progress in lab3

--- lab3/matrix.py
+++ lab3/matrix.py
@@ -1,15 +1,10 @@
 def transpose(x):
-	y = [[0] * len(x)] * len(x[0])
-	i = 0
-	for row in x:
-		j = 0
-		for elem in row:
-			print("j = ", j, ", i = ", i)
-			print(elem)
-			y[j][i] = elem
-			j += 1
-			print(y)
-		i += 1
+	y = [] 
+	for i in range(len(x[0])):
+		z = []
+		for j in range(len(x)):
+			z.append(x[j][i])
+		y.append(z)
 	return y
 
 
